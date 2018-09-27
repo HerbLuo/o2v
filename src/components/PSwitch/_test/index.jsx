5f9bc26b488e6122104d4cbb0c46b26b221e4338
herbluo <cloudself.cn@gmail.com>
2018-09-27 22:20:48 +0800

    优化了PSwitch的测试用例

--- src/components/PSwitch/_test/index.jsx
+++ src/components/PSwitch/_test/index.jsx
@@ -4,7 +4,7 @@ import { PSwitch } from '@lib'
 export default class PSwitchTest extends React.Component {
   render () {
     return <PSwitch
-      className="dev"
+      className="___warning_dev"
       checked={true}
       style={{marginTop: '10px'}}
     />
